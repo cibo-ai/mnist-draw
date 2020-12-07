39e3218e957fb1f1d6d3e698a7020ca1c0b4a2ca
crow <crow@nest>
2020-12-07 19:34:25 +1100

    cleanup

--- cgi-bin/mnist.py
+++ cgi-bin/mnist.py
@@ -17,18 +17,6 @@ from torchvision import transforms
 from scipy import ndimage
 import math
 
-# Default output
-res = {"result": 0,
-       "data": [], 
-       "error": '',
-       "output": ''}
-
-# Load the model
-model = torch.load('/home/crow/handwritten-digit/models/conv2d_model.pt')
-model.eval()
-# Change the model to double type because image is double for some reason
-model.double()
-
 def getBestShift(image):
   # Get the shift value for the transformation matrix
   cy, cx = ndimage.measurements.center_of_mass(image)
@@ -65,49 +53,64 @@ def preprocessImage(image):
   #image = shift(image, shiftx, shifty)
   return image
 
-try:
-  # Get post data
-  if os.environ["REQUEST_METHOD"] == "POST":
-    data = sys.stdin.read(int(os.environ["CONTENT_LENGTH"]))
 
-    # Convert data url to numpy array
-    img_str = re.search(r'base64,(.*)', data).group(1)
-    image_bytes = io.BytesIO(base64.b64decode(img_str))
-    im = Image.open(image_bytes)
-    image = np.array(im)[:,:,0]
-    image = np.reshape(image, (28, 28))
+if __name__ == "__main__":
+  # Default output
+  res = {"result": 0,
+         "data": [], 
+         "error": '',
+         "output": ''}
 
-    # Normalize and invert pixel values to range [-1, 1]
-    image = -1 + (255. - image) * 2 / 255.
+ # Load the model
+  model = torch.load('/home/crow/handwritten-digit/models/conv2d_model.pt')
+  model.eval()
+  # Change the model to double type because image is double for some reason
+  model.double()
 
-    # Preprocess the image to look like MNIST training dataset
-    image = preprocessImage(image)
+  try:
+    # Get post data
+    if os.environ["REQUEST_METHOD"] == "POST":
+      data = sys.stdin.read(int(os.environ["CONTENT_LENGTH"]))
 
-    # transform numpy array into a tensor
-    image = torch.from_numpy(image)
-    image = image.unsqueeze(0)
-    image = image.unsqueeze(0)
+      # Convert data url to numpy array
+      img_str = re.search(r'base64,(.*)', data).group(1)
+      image_bytes = io.BytesIO(base64.b64decode(img_str))
+      im = Image.open(image_bytes)
 
-    # get the prediction from the model
-    with torch.no_grad():
-      logps = model(image)
+      # Transform the image data into numpy array
+      image = np.array(im)[:,:,0]
+      image = np.reshape(image, (28, 28))
 
-    # get the prediction result in list of probable from 0-9
-    ps = torch.exp(logps)
-    probab = list(ps.numpy()[0])
+      # Normalize and invert pixel values to range [-1, 1]
+      image = -1 + (255. - image) * 2 / 255.
 
-    res['result'] = 1
-    res['data'] = probab
-    
-    # output to the browser dev console for debugging
-    res['output'] = str(image) 
+      # Preprocess the image to look like MNIST training dataset
+      image = preprocessImage(image)
 
-except Exception as e:
-  # Return error data
-  res['error'] = str(e)
+      # transform numpy array into a 4-d tensor 
+      image = torch.from_numpy(image).unsqueeze(0).unsqueeze(0)
 
-# Print JSON response
-print("Content-type: application/json")
-print("") 
-print(json.dumps(res))
+      # get the prediction from the model
+      with torch.no_grad():
+        logps = model(image)
+
+      # get the prediction result in list of probable from 0-9
+      ps = torch.exp(logps)
+      probab = list(ps.numpy()[0])
+
+      # Return the result to localhost
+      res['result'] = 1
+      res['data'] = probab
+      
+      # output to the browser dev console for debugging
+      res['output'] = str(image) 
+
+  except Exception as e:
+    # Return error data
+    res['error'] = str(e)
+
+  # Print JSON response
+  print("Content-type: application/json")
+  print("") 
+  print(json.dumps(res))
 
