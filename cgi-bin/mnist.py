71dac87c27a28941f5c5463988dff66dc3efb525
crow <crow@nest>
2020-12-24 20:54:09 +1100

    fix the AttributeError for model loading, solution right now is a bit bad

--- cgi-bin/mnist.py
+++ cgi-bin/mnist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 CGI script that accepts image urls and feeds them into a ML classifier. Results
 are returned in JSON format. 
@@ -13,9 +13,42 @@ import numpy as np
 from PIL import Image
 import torch
 import torchvision
+from torch import nn
+import torch.nn.functional as F
 from torchvision import transforms
 from scipy import ndimage
 import math
+
+
+class Model(nn.Module):
+  def __init__(self):
+    super(Model, self).__init__()
+    # Convolutional layers
+    self.conv1 = nn.Conv2d(in_channels=1, out_channels=12, kernel_size=3, stride=1, padding=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(12, affine=True)
+    self.conv2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=6, stride=2, padding=2, bias=False)
+    self.bn2 = nn.BatchNorm2d(24, affine=True)
+    self.conv3 = nn.Conv2d(in_channels=24, out_channels=32, kernel_size=6, stride=2, padding=2, bias=False)
+    self.bn3 = nn.BatchNorm2d(32, affine=True)
+    
+    # Dense layers
+    self.fc1 = nn.Linear(in_features=32*7*7, out_features=200, bias=False)
+    self.bn4 = nn.BatchNorm1d(200, affine=True)
+    self.dr = nn.Dropout(p=0.3)
+    self.fc2 = nn.Linear(in_features=200, out_features=10, bias=False)
+  
+  def forward(self, x):
+    x = F.relu(self.bn1(self.conv1(x)))
+    x = F.relu(self.bn2(self.conv2(x)))
+    x = F.relu(self.bn3(self.conv3(x)))
+    
+    # Flatten the input to feed into the dense layers
+    x = x.flatten(start_dim=1)
+    
+    x = F.relu(self.bn4(self.fc1(x)))
+    x = self.dr(x)
+    x = F.log_softmax(self.fc2(x), dim=1)
+    return x
 
 def getBestShift(image):
   # Get the shift value for the transformation matrix
@@ -62,7 +95,8 @@ if __name__ == "__main__":
          "output": ''}
 
  # Load the model
-  model = torch.load('/home/crow/handwritten-digit/models/conv2d_model.pt')
+  model = Model()
+  model.load_state_dict(torch.load('./models/conv2d_model.pt'))
   model.eval()
   # Change the model to double type because image is double for some reason
   model.double()
